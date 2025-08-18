b1de264adf968a37de2c2b825bdb3464a9e64ad0
pxlmover <125728788+pxlmover@users.noreply.github.com>
2025-08-18 00:07:08 -0700

    nightcommit

---
+++ GuideAR/common.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const sig = document.createElement('div');
+  sig.className = 'site-signature';
+  sig.textContent = 'Created In Unreal Engine By Daniel Jensen - 2025';
+  document.body.appendChild(sig);
+});
